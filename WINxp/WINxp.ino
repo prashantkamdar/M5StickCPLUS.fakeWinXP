c229725d93b6133c3d2e8c3549108d0bc20efc5f
prashantkamdar <prashantkamdar@gmail.com>
2021-12-28 17:10:14 +0530

    desktop working

--- WINxp/WINxp.ino
+++ WINxp/WINxp.ino
@@ -3,16 +3,21 @@
 #include "welcome.h"
 #include "error.h"
 #include "bsod.h"
+#include "desktop.h"
+
+bool showerror = true;
 
 void setup() {
+  pinMode(37, INPUT_PULLUP);
   M5.begin();
-  M5.Lcd.setRotation(0);
-  M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setSwapBytes(true);
 }
 
 void loop() {
-  for (int i = 0; i < 9; i++)
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setRotation(0);
+
+  for (int i = 0; i < 3; i++)
     for (int i = 0; i < 11; i++) {
       M5.Lcd.pushImage(0, 50, 135, 136, winxploading[i]);
       delay(80);
@@ -21,11 +26,18 @@ void loop() {
   M5.Lcd.pushImage(0, 0, 135, 240, welcome);
   delay(5200);
 
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.pushImage(0, 0, 135, 240, error);
-  delay(4000);
+  if (showerror) {
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.pushImage(0, 80, 135, 83, error);
+    delay(4000);
 
-  M5.Lcd.pushImage(0, 0, 135, 240, bsod);
-  delay(7000);
-  M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.pushImage(0, 70, 135, 100, bsod);
+    
+    showerror = false;
+  } else {
+    M5.Lcd.setRotation(3);
+    M5.Lcd.pushImage(0, 0, 240, 135, desktop);
+    showerror = true;
+  }
+  while (digitalRead(37) == 1) {}
 }
