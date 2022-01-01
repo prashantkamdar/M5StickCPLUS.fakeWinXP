6c7314391ea8d9ba823dc5be80f29126dbb51065
prashantkamdar <prashantkamdar@gmail.com>
2022-01-01 13:51:36 +0530

    all done

--- WINxp/WINxp.ino
+++ WINxp/WINxp.ino
@@ -8,7 +8,6 @@
 bool showerror = true;
 
 void setup() {
-  pinMode(37, INPUT_PULLUP);
   M5.begin();
   M5.Lcd.setSwapBytes(true);
 }
@@ -18,26 +17,57 @@ void loop() {
   M5.Lcd.setRotation(0);
 
   for (int i = 0; i < 3; i++)
-    for (int i = 0; i < 11; i++) {
-      M5.Lcd.pushImage(0, 50, 135, 136, winxploading[i]);
+    for (int j = 0; j < 11; j++) {
+      M5.Lcd.pushImage(0, 50, 135, 136, winxploading[j]);
       delay(80);
     }
 
   M5.Lcd.pushImage(0, 0, 135, 240, welcome);
-  delay(5200);
+  delay(3500);
 
   if (showerror) {
     M5.Lcd.fillScreen(BLACK);
     M5.Lcd.pushImage(0, 80, 135, 83, error);
-    delay(4000);
+    delay(3500);
 
     M5.Lcd.pushImage(0, 70, 135, 100, bsod);
-    
+
     showerror = false;
   } else {
     M5.Lcd.setRotation(3);
     M5.Lcd.pushImage(0, 0, 240, 135, desktop);
+
     showerror = true;
+
+    delay(750);
+    M5.Beep.tone(1175, 125);
+    delay(125);
+    M5.Beep.end();
+    delay(125);
+
+    M5.Beep.tone(587, 125);
+    delay(125);
+    M5.Beep.end();
+
+    M5.Beep.tone(880, 125);
+    delay(125);
+    M5.Beep.end();
+    delay(250);
+
+    M5.Beep.tone(784, 125);
+    delay(125);
+    M5.Beep.end();
+    delay(375);
+
+    M5.Beep.tone(1175, 125);
+    delay(125);
+    M5.Beep.end();
+    delay(125);
+
+    M5.Beep.tone(880, 1000);
+    delay(1000);
+    M5.Beep.end();
   }
+
   while (digitalRead(37) == 1) {}
 }
